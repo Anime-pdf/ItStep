d1c53fcc9a769c3ab65252d4ba03ed8b76853a54
Paul <55580385+Anime-pdf@users.noreply.github.com>
2021-03-03 21:00:27 +0200

    2pr

---
+++ Classwork/2_para/2_para/2_para/2_para.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "rus");
+    int first_number, second_number;
+    cout << "Введите первое число: ";
+    cin >> first_number;
+	cout << "Введите второе число: ";
+    cin >> second_number;
+
+    cout << endl << "Ваши числа: " << first_number << ", " << second_number;
+
+    cout << "\
+        Результат сложения:" << first_number + second_number << "\n\
+        Результат отнимания:" << first_number - second_number << "\n\
+        Результат умножения:" << first_number * second_number << "\n\
+        Результат деления:" << first_number / second_number << "\n";
+
+    system("pause");
+    return 1;
+}
