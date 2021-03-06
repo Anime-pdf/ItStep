eff4012073a5eed48c56d4e13c6fa446debe7817
Paul <55580385+Anime-pdf@users.noreply.github.com>
2021-03-06 20:38:23 +0200

    hw2

---
+++ Homework/3.03.2021/hw/hw/hw.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cout << "Enter first number (a): ";
+    cin >> a;
+    cout << "Enter second number (b): ";
+    cin >> b;
+	cout << "Enter third number (c): ";
+    cin >> c;
+
+    cout << "a * b * c = " << a * b * c << endl;
+	cout << "a + b + c = " << a + b + c << endl;
+
+    system("pause");
+}
