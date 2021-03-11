eb95aebd5e89325f0548bdc992d064d8cb7735a5
Paul <55580385+Anime-pdf@users.noreply.github.com>
2021-03-11 16:04:30 +0200

    3p

---
+++ Classwork/3_para/para3/para3/para3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\t===CALCULATOR===\n1.add\n2.subtract\n3.multiplication\n4.division\n";
+
+    int operation, first_number, second_number;
+
+    cout << "What do u waht to use (Enter number): ";
+    cin >> operation;
+
+	cout << "Enter first number: ";
+    cin >> first_number;
+	cout << "Enter second number: ";
+    cin >> second_number;
+    if (second_number==0 && operation==4)
+    {
+        cout << "no";
+        return -1;
+    }
+
+    switch (operation)
+    {
+    case 1:
+        cout << "Result:" << first_number + second_number;
+        break;
+    case 2:
+		cout << "Result:" << first_number - second_number;
+        break;
+    case 3:
+		cout << "Result:" << first_number * second_number;
+        break;
+    case 4:
+		cout << "Result:" << first_number / second_number;
+        break;
+    default:
+		cout << "no";
+        return -1;
+        break;
+    }
+
+    system("pause");
+    return 1;
+}
