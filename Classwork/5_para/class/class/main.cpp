838dd6765995d8a79e72ded247d8e8fd44421523
Paul <55580385+Anime-pdf@users.noreply.github.com>
2021-03-24 17:39:14 +0200

    first n second

---
+++ Classwork/5_para/class/class/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+bool isSame(int a)
+{
+	if (a / 100 == a % 10 || a / 100 == a % 100 / 10 || a % 100 / 10 == a % 10)
+		return true;
+	else
+		return false;
+}
+
+int main()
+{
+	int first_count = 0, second_count = 0;
+	for (int i = 100; i < 1000; i++)
+	{
+		if (isSame(i))
+		{
+			first_count++;
+		}
+		else
+			second_count++;
+	}
+	system("pause");
+	return 0;
+}
